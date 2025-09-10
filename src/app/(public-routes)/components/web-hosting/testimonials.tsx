04a3a41bb6e6d0d0e70609a5d028ba85da2c2da5
ssmahim01 <mahimzayn666@gmail.com>
2025-09-11 02:53:53 +0600

    Modified: testimonials section

--- src/app/(public-routes)/components/web-hosting/testimonials.tsx
+++ src/app/(public-routes)/components/web-hosting/testimonials.tsx
@@ -166,7 +166,7 @@ export function TestimonialsSection() {
                 {testimonials.map((testimonial) => (
                   <SwiperSlide key={testimonial.id}>
                     <Card className="border-0 shadow-lg bg-white dark:bg-gray-800 p-0">
-                      <CardContent className="p-4">
+                      <CardContent className="p-6">
                         <div className="flex items-start gap-4">
                           <Avatar className="w-16 h-16 flex-shrink-0">
                             <AvatarImage
