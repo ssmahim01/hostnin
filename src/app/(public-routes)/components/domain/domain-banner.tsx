aec9680ebfefcd6dcebd631d3d9a9a52babeaff7
ssmahim01 <mahimzayn666@gmail.com>
2025-09-14 17:50:17 +0600

    Updated: domain-banner

--- src/app/(public-routes)/components/domain/domain-banner.tsx
+++ src/app/(public-routes)/components/domain/domain-banner.tsx
@@ -79,7 +79,7 @@ export default function DomainBanner() {
             <Input
               type="text"
               name="query"
-              className="rounded-l-lg rounded-r-none py-8 flex-1 border-none focus:ring-0 focus:border-blue-500 bg-white/90 placeholder-white/80 text-gray-700 font-medium"
+              className="rounded-l-lg rounded-r-none py-8 flex-1 border-none  placeholder-white/80 text-gray-700 font-medium"
               placeholder="Enter your domain name..."
               value={domain}
               onChange={(e) => setDomain(e.target.value)}
