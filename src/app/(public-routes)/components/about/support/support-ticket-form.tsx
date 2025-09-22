2468b517d57ee9b0b6ff477485bf7f98fdefe5cd
ssmahim01 <mahimzayn666@gmail.com>
2025-09-22 22:23:21 +0600

    Updated: support-ticket form

--- src/app/(public-routes)/components/about/support/support-ticket-form.tsx
+++ src/app/(public-routes)/components/about/support/support-ticket-form.tsx
@@ -152,7 +152,7 @@ export function SupportTicketForm() {
       errorMessage: "",
       credentialEmail: "",
       credentialLogin: "",
-      credentialUrl: "",
+      credentialUrl: "https://my.hostnin.com/index.php/login",
     },
   });
 
@@ -567,6 +567,8 @@ ${data.email || ""}
                       <FormControl>
                         <Input
                           type="url"
+                          readOnly
+                          defaultValue={field.value}
                           placeholder="https://my.hostnin.com/index.php/login"
                           {...field}
                         />
