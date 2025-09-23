c1fa96c4defc61f73bbbc47ed1fe0eaa0110242c
ssmahim01 <mahimzayn666@gmail.com>
2025-09-24 02:31:00 +0600

    Changed image from advanced-features section

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "photos.app.goo.gl",
+        pathname: "/**",
+      },
     ],
   },
 };
--- src/app/(public-routes)/components/web-hosting/advanced-features.tsx
+++ src/app/(public-routes)/components/web-hosting/advanced-features.tsx
@@ -68,7 +68,7 @@ export default function AdvancedFeatures() {
           {/* Illustration Image */}
           <div className="md:w-2/5 flex justify-center md:justify-start">
             <Image
-              src="/assets/advanced-feature.jpg"
+              src="/assets/team-hostnin.jpg"
               alt="Advanced features illustration"
               width={900}
               height={800}
